51d9b932bc5502901550bebb710fc30d22dc10e3
kendylopes <kendylopes@gmail.com>
2026-04-02 21:03:54 -0400

    feat: Criando Player 1 e Player 2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-console.log("Olá mundo, Kennedy")
-console.log("Vamos pra cima com tudo")
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+};
+
+const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+};
